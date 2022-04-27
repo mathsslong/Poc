a4f61c76e572d94a5899bc9e9796793f19ea2ac7
sunshanlong <2736908546@qq.com>
2022-04-27 16:38:56 +0800

    improve

--- src_tiqu.py
+++ src_tiqu.py
@@ -1,24 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
 import requests
 from lxml import etree
 
-#yeshu=input("您想要搞多少页:")
-
 def src_tiqu(yeshu):
-    for i in range(1, int(yeshu+1)):
-        url = 'https://src.sjtu.edu.cn/list/?page=' + str(i)
-        print("提取->",str(i)+"页数")
-        data = requests.get(url).content
-        print(data.decode('utf-8'))
-        soup = etree.HTML(data)
-        result = soup.xpath('//td[@class=" "]/a/text()')
-        results = '\n'.join(result)
-        resultss = results.split()
-        # print(resultss)
-        # 将结果写入文件
-        for edu in resultss:
-            print(edu)
-            with open(r'src_edu.txt', 'a+', encoding='utf-8') as f:
-                f.write(edu + '\n')
+	for i in range(1,int(yeshu)+1):
+		url='https://src.sjtu.edu.cn/list/?page=' + str(i)
+		print("提取---->",str(i)+"页数")
+		data=requests.get(url).content
+		print(data.decode('utf-8'))
+		soup = etree.HTML(data)
+		result=soup.xpath('//td/a/text()')
+		results = '\n'.join(result)
+		resultss = results.split()
+		print(resultss)
+		for edu in resultss:
+			with open(r'src_edu.txt','a+',encoding='utf-8') as f:
+				print(edu)
+				f.write(edu+'\n')
+				
 
 
 if __name__ == '__main__':
